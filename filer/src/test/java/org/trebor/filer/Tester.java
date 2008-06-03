d13e7f72f3fff6016b49b2386bf7212959df7654
robertomarin <robertomarin@55e2884b-8b4e-0410-b7d9-e196465f43e6>
2008-06-03 03:36:13 +0000

    test regex

--- filer/src/test/java/org/trebor/filer/Tester.java
+++ filer/src/test/java/org/trebor/filer/Tester.java
@@ -5,14 +5,10 @@ import java.io.Console;
 public class Tester {
 
 	public static void main(String[] args) {
-		Console console = System.console();
 	
-		if (console != null) {
-			String readLine = console.readLine();
-			
-			System.out.println("linha: " + readLine);
-		}
+		String s = "motörhead";
 		
+		System.out.println(s.replaceAll("(motörhead)", "$1sss"));
 
 	}
 }
